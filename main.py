4139936f4ab7262c5ae432aa889345c77f738052
23f2003584 <23f2003584@ds.study.iitm.ac.in>
2025-06-14 16:02:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -69,5 +69,7 @@ def home():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+
+    port = int(os.environ.get("PORT", 8000))  # Railway will set this
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
