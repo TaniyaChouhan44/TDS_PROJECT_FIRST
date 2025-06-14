e431bae1c113a9c2dd28b82869e1b4540ec267e9
23f2003584 <23f2003584@ds.study.iitm.ac.in>
2025-06-14 18:44:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -75,8 +75,12 @@ def home():
 
 
 if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))  # Railway provides dynamic PORT
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True) 
+
+
+#if __name__ == "__main__":
+#    import uvicorn
+#    port = int(os.environ.get("PORT", 8000))  # Railway provides dynamic PORT
+#    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
   
