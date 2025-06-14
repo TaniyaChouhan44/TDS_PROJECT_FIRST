8e1cf3d297a7b632b8db2c9846d7150f593f6a55
Taniyachouhaniitm <taniyachouhan303@gmail.com>
2025-06-14 17:41:32 +0530

    Update changes

--- main.py
+++ main.py
@@ -68,14 +68,9 @@ def home():
     return {"message": "TDS Virtual TA is running!"}
 
 
-'''if __name__ == "__main__":
-     import uvicorn
-     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-
-
 if __name__ == "__main__":
-   
-    port = int(os.environ.get("PORT", 8000))  # Railway will set this
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # Railway provides dynamic PORT
     uvicorn.run("main:app", host="0.0.0.0", port=port)
 
-'''    
+  
